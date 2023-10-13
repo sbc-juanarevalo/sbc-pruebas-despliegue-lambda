adecc8a6db6bb7c5a61517e7b36ed92181ed0c86
sbc-juanarevalo <juanarevalo@subocol.com>
2023-10-12 20:18:20 -0500

    Commit 2

--- login/src/domain/ports/storageport.py
+++
@@ -1,5 +0,0 @@
-class StoragePort:
-    
-    def find_risk_by_city(self, id):
-        print (id)
-        pass
--- login/src/domain/services/mainservice.py
+++
@@ -1,6 +0,0 @@
-class MainService:
-    
-    def __init__(self, storage, client):
-        self.storage = storage
-        self.client = client
-        pass
