fbfc7bd8c41e51183cb30897915a6b143c532df7
sbc-juanarevalo <juanarevalo@subocol.com>
2023-12-04 13:11:02 -0500

    Commit 60

--- login/src/JuanFelipePruebas2/infrastucture/dynamodbadapter.py
+++
@@ -1,7 +0,0 @@
-from domain.ports.storageport import StoragePort
-
-
-class DynamoDBAdapter(StoragePort):
-    
-    def find_risk_by_city(self, id):
-        pass
--- login/src/JuanFelipePruebas2/lambda_function.py
+++
@@ -1,6 +0,0 @@
-from application.maincontroller import MainController
-
-
-def lambda_handler(event, context):
-    main = MainController()
-    return main.main_lambda(event)
